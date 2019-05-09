fe934071afefc0fdaf1f68bd342c0a862ae23b74
lustupuce <luluhouel@hotmail.com>
2019-05-09 11:44:01 +0200

    Update RobotController.h

--- simulation/src/RobotController.h
+++ simulation/src/RobotController.h
@@ -1,11 +1,11 @@
-/*
- * RobotController.h
- * This class act as a supervisor for the Robots.
- * It handles the collective behavior of the robots and the synchronization between them.
- * The individual Robot object should be created via the RobotControlller.
- * If it is not the case, the robot can be added afterward to the robotController via the addRobot(Robot& robot) method.
- *  Created on: 28 sept. 2018
- *      Author: lucie
+/**
+ *	@file RobotController.h
+ *	@brief Implement the RobotController class. This class act as a supervisor for the Robots.
+ *	@note It handles the collective behavior of the robots and the synchronization between them.
+ *	The individual Robot object should be created via the RobotControlller.
+ *	If it is not the case, the robot can be added afterward to the robotController via the addRobot(Robot& robot) method.
+ *	@date 26 sept. 2018
+ *	@author lucie houel
  */
 
 #ifndef ROBOTCONTROLLER_H_
@@ -13,22 +13,36 @@
 
 #include "Robot.h"
 
-/*----------------------TODOO
- * create a function to assign a robot to a controller after the robot has been created
- * */
-
-enum direction {CLOCK_WISE, COUNTER_CLOCK_WISE};
-
 class RobotController {
 public:
 	RobotController();
 	RobotController(config::sController controllerParam, config::sRobot robotParam, b2Body* terrain, double m_to_px = 0);
 	virtual ~RobotController();
 
-	void setNbRobots(int nb_robots);
-	void setScale(double m_to_px);
+	/**
+	 * Add a robot to the controller after the robot has been created.
+	 * A better solution to add a robot to the simulation should be to create directly a new robot
+	 * from the controller method using the createRobot method.
+	 * @param robot is a pointer on a newly created robot object.
+	 */
+	void addRobot(Robot* robot);
+
+	/**
+	 * Check if the robot is grabbed by any other robot of the controller
+	 * @param robot is the robot to be tested
+	 * @return true if the robot is grabbed, false otherwise
+	 */
+	bool checkGrabbed(Robot& robot);
 
 	void create(config::sController controllerParam, config::sRobot robotParam, b2Body* terrain, double m_to_px = 0);
+
+	/**
+	 * Takes the robot as parameter and create a new grip in the box2D world if the robot.isContact() is true
+	 * @param 	robot generally from the m_robotVector of the controller
+	 * @update 	Robot::m_previousGripJoint and Robot::m_currentGripJoint for contacted robots
+	 * @update 	Robot::m_contact by setting it to false once the joint has been created
+	 */
+	void createGripRobots(Robot& robot);
 
 	/**
 	 * The function create a new robot object and add it to the m_robotVector
@@ -42,16 +56,11 @@ public:
 	bool createRobot(b2World* world, int delay, double posX, double posY, double angle = 0);
 
 	/**
-	 * @param 	pos_id the robot position in the m_robotVector Robot vector containing all the robots
-	 * belonging to the RobotController. The pos_id starts at 0
-	 * Be careful pos_id does not necessarily match the robot id
-	 * @return 	a pointer on the robot object */
-	Robot* getRobot(int pos_id);
-	/**
-	 * @param 	id is the robot unique id
-	 * Be careful, it does not necessarily match the robot position within the robots' vector.
-	 * @	return a pointer on the robot object */
-	Robot* getRobotWithId(int id);
+	 * Remove from the world and destroy a given Box2D joint. This joint is one of a robot gripper
+	 * @param 	s is either LEFT or RIGHT
+	 * @update	Robot::m_previousGripJoint and Robot::m_currentGripJoint
+	 */
+	void destroyJoints(Robot& robot, side s);
 
 	/**
 	 * The function draws all the robots of the Robot controller on the SFML window
@@ -72,37 +81,53 @@ public:
 	void findContactRobots(b2Contact* contact);
 
 	/**
-	 * Takes the robot as parameter and create a new grip in the box2D world if the robot.isContact() is true
-	 * @param 	robot generally from the m_robotVector of the controller
-	 * @update 	Robot::m_previousGripJoint and Robot::m_currentGripJoint for contacted robots
-	 * @update 	Robot::m_contact by setting it to false once the joint has been created
+	 * Get the time required to obtain the dissolution of the bridge.
+	 * The dissolution is reached when no more robots are in the bridge state
+	 * @return the simulated time to reach the dissolution
 	 */
-	void createGripRobots(Robot& robot);
+	double getDissolutionTime();
 
 	/**
-	 * Handle the waiting delay of the robots by decreasing it at each world step.
-	 * When the delay is null, the robot.m_ready state is set to true
-	 * @param 	robot generally from the m_robotVector of the controller
-	 * @update	Robot::m_ready is set to true when Robot::m_delay is null
-	 * @update	Robot::m_delay is decreased otherwise
+	 * Get the number of active robots of the robotController.
+	 * ie the number of robots in the world window
+	 * @return the number of active robots
 	 */
-	void wait_delay(Robot& robot);
+	int getNbActiveRobots();
 
 	/**
-	 * Run a step of the simulation.
-	 * @param 	end_x is the position of the end of the simulation world
-	 * 			it generally correspond to the end of the window.
-	 * 			above this position the robot is destroyed.
+	 * Get the number of robots of the robotController under a certain state
+	 * @param s is the considered state, can be either BRIDGE or WALK
+	 * @return the number of robot in this given state
 	 */
-	void step(double end_x);
+	int getNbRobots(e_state s);
 
 	/**
-	 * Function used to set the state of a specific robot
-	 * @param 	robot is the Robot object
-	 * @param	robotState can be either WALK or BRIDGE
-	 * @update	Robot::m_moving, Robot::m_state, Robot::m_contact and Robot::m_delay
+	 * Get the number of robot still active at the end of the simulation
+	 * @return the number of robots still active at the time of the function call.
 	 */
-	void setRobotState(Robot& robot,  e_state robotState);
+	int getNbRobotsBlocked();
+
+	/**
+	 * @param 	pos_id the robot position in the m_robotVector Robot vector containing all the robots
+	 * belonging to the RobotController. The pos_id starts at 0
+	 * Be careful pos_id does not necessarily match the robot id
+	 * @return 	a pointer on the robot object */
+	Robot* getRobot(int pos_id);
+
+	/**
+	 * @param 	id is the robot unique id
+	 * Be careful, it does not necessarily match the robot position within the robots' vector.
+	 * @	return a pointer on the robot object */
+	Robot* getRobotWithId(int id);
+
+	/**
+	 * Get the time required to obtain a stable bridge.
+	 * A stable bridge is reached when the stability condition is reached.
+	 * This condition is expressed as a duration defined by m_controllerParam.stability_condition during which no more robots enter the bridge state.
+	 * This duration can be changed in the main file or as a parameter in the simulation launch.
+	 * @return the simulated time to reach stability
+	 */
+	double getStabilizationTime();
 
 	/**
 	 * Invert the moving side of the robot
@@ -112,43 +137,10 @@ public:
 	void invertMovingWheel(Robot& robot);
 
 	/**
-	 * Remove from the world and destroy a given Box2D joint. This joint is one of a robot gripper
-	 * @param 	s is either LEFT or RIGHT
-	 * @update	Robot::m_previousGripJoint and Robot::m_currentGripJoint
+	 * Check if the bridge has been dissolved. We consider that the dissolution is reached if only one robot remains in the simulation.
+	 * @return true if the dissolution of the bridge is obtained, false otherwise
 	 */
-	void destroyJoints(Robot& robot, side s);
-
-	/**
-	 * Add/Remove a robot to the controller
-	 */
-	void addRobot(Robot* robot);
-	void removeRobot();
-
-	/**
-	 * Check if a robot with a given unique id is outside the world.
-	 * If it is the robot is removed from the world and destroyed
-	 * @param 	end_x corresponds to the end of the world
-	 * @param	id is the unique id of the robot, can be obtained with robot.getID()
-	 */
-	void robotOut(double end_x, int id);
-
-	/**
-	 * Check if the robot is grabbed by any other robot of the controller
-	 * @return true if the robot is grabbed, false otherwise
-	 */
-	bool checkGrabbed(Robot& robot);
-
-	/**
-	 * Get the time required to obtain a stable bridge
-	 * @return the simulated time
-	 */
-	double getStabilizationTime();
-
-	/**
-	 * Get the time required to obtain a stable bridge
-	 * @return the simulated time
-	 */
-	double getDissolutionTime();
+	bool isBridgeDissolved();
 
 	/**
 	 * Check if a stable bridge has been obtained. To do so, we compare the time the last robot entered the bridge with the stability condition.
@@ -159,31 +151,21 @@ public:
 	bool isBridgeStable();
 
 	/**
-	 * Check if the bridge has been dissolved. We consider that if only one robot is remained, the dissolution has been reached.
-	 * @return true if the dissolution of the bridge is obtained, false otherwise
+	 * Remove robots from the controller and call its destructor.
+	 * The robots that are destroyed using this method are the ones whose id has been copied
+	 * inside the m_robotToDestroy vector.
+	 * @update at the end all the robots whose id is in the m_robotToDestroy vector are destroyed and this vector is emptied
 	 */
-	bool isBridgeDissolved();
+	void removeRobot();
 
 	/**
-	 * Set if a stable bridge has been obtained
-	 * @param stable is either true if the stability has been reached or false
-	 * @update m_isStable
+	 * Check if a robot with a given unique id is outside the world.
+	 * If it is the robot id is copied into the m_robotToDestroy vector to be destroyed at the next call
+	 * of the removeRobot() method
+	 * @param 	end_x corresponds to the end of the world
+	 * @param	id is the unique id of the robot, can be obtained with robot.getID()
 	 */
-	void setBridgeStability(bool stable);
-
-	/**
-	 * Get the number of robots of the robotController under a certain state
-	 * @param s is the considered state, can be either BRIDGE or WALK
-	 * @return the number of robot in this given state
-	 */
-	int getNbRobots(e_state s);
-
-	/**
-	 * Get the number of active robots of the robotController.
-	 * ie the number of robots in the world window
-	 * @return the number of active robots
-	 */
-	int getNbActiveRobots();
+	void robotOut(double end_x, int id);
 
 	/**
 	 * Determine if a robot is pushing after the pushing delay has been reached
@@ -199,46 +181,95 @@ public:
 
 	/**
 	 * This function is used to help determine if the robots are stacking.
-	 * It does so by checking if there is a robot located before the given position posX.
+	 * It is called when a new robot will be created to check if the previous robot is too close to the new robot.
+	 * The previous robot is too close when it is located before the given position posX + one bodyLength unit.
 	 * @param r is the Robot to be checked
 	 * @param posX is the superior limit of the position one wants to check
 	 * @return true if the robot is located before posX, false otherwise
 	 */
 	bool robotStacking(Robot* r, float posX);
 
-	int getNbRobotsBlocked();
+	/**
+	 * Change the bridge stability flag value.
+	 * @param stable is either true if the stability has been reached or false
+	 * @update m_isStable
+	 */
+	void setBridgeStability(bool stable);
 
+	/**
+	 * Set the maximum number of robots that can be active at the same time in the simulation window.
+	 * It is used to limit the size of the m_robotVector.
+	 * @param 	nb_robots is the maximum number of robot that can be existing at the same time.
+	 * */
+	void setNbRobots(int nb_robots);
+
+	/**
+	 * Function used to set the state of a specific robot
+	 * @param 	robot is the Robot object
+	 * @param	robotState can be either WALK or BRIDGE
+	 * @update	Robot::m_moving, Robot::m_state, Robot::m_contact and Robot::m_delay
+	 */
+	void setRobotState(Robot& robot,  e_state robotState);
+
+	/**
+	 * Set the scale to do the conversion from real dimensions (m) to simulated ones (pixels).
+	 * Ususlly the scale is obtained from the terrain with the Terrain::getScale() method.
+	 * @param 	m_to_px is the conversion factor from meters to pixels
+	 * */
+	void setScale(double m_to_px);
+
+	/**
+	 * Run a step of the simulation.
+	 * @param 	end_x is the position of the end of the simulation world
+	 * 			it generally correspond to the end of the window.
+	 * 			above this position the robot is destroyed.
+	 */
+	void step(double end_x);
+
+	/**
+	 * Handle the waiting delay of the robots by decreasing it at each world step.
+	 * When the delay is null, the robot.m_ready state is set to true
+	 * @param 	robot generally from the m_robotVector of the controller
+	 * @update	Robot::m_ready is set to true when Robot::m_delay is null
+	 * @update	Robot::m_delay is decreased otherwise
+	 */
+	void wait_delay(Robot& robot);
+
+	/**
+	 * Flag used to notify when a new robot enter the bridge state.
+	 * It is used to write a new entry in the bridge file.
+	 * It is set to false once the bridge file have been updated (in the demo.cpp file).
+	 */
 	bool m_bridgeEntry = false;
 
 
 private:
-	std::vector<Robot*> m_robotVector;
-	std::vector<int> m_robotToDestroy;
-	int m_nbRobots = 0;
-	int m_currentIt = 0;
+
+	std::vector<Robot*> m_robotVector; // Vector containing a pointer on every robot object attached to this robot controller
+	std::vector<int> m_robotToDestroy; // Vector containing the id of all the robot that have to be destroyed during the next simulation step
+	int m_nbRobots = 0; // Number of robot attached to this robot controller
+	int m_currentIt = 0; // Current iteration
 	int m_idLastRobot = 0; // id of the last robot that entered the bridge state
-	int m_nbRobInBridge = 0;
-	double m_stableTime = 0;
-	bool m_isStable = false;
-	bool m_isDissolved = false;
-	double m_dissolutionTime = 0;
+	int m_nbRobInBridge = 0; // Number of robot in the bridge state
+	double m_stableTime = 0; // Time to reach stable bridge (real time)
+	bool m_isStable = false; // Flag to indicate bridge stability
+	bool m_isDissolved = false; // Flag to indicate if the bridge dissolved
+	double m_dissolutionTime = 0; // Time to reach bridge dissolution (real time)
+
+	/** Angle minimum (rad) the robot has to run to be able to grab. Below this angle the robot cannot create a grip even after the pushing delay.
+	 * This angle prevent the robot to grip again its previous position.
+	 * Rq: This angle could be adapted to fit 1 or 2 times the angular resolution of the robot rotation
+	 * which is determined by the ratio between the time step and the robot angular speed: angular_resolution = angular_speed (rad/s) / time_step (s)
+	 */
 	double m_angle_min= PI/8;
-	b2Body* m_terrainBody=nullptr;
 
-//
-	config::sController m_controllerParam;
-	config::sRobot m_robotParam;
+	b2Body* m_terrainBody=nullptr; //Pointer on the terrain Box2D body
 
-//	const int m_DELAY_BRIDGE_UP = 30;
-//	const int m_DELAY_BRIDGE_DOWN = 300;
-//	const int m_DELAY_WALK = 30;
-//	const int m_TIME_TO_MOVE =300;
-//	const int m_ANGLE_LIM = PI/2;
-	int m_M_TO_PX;
+	config::sController m_controllerParam; //Structure containing the controller parameters. The structure is defined in the Config.h file
+	config::sRobot m_robotParam; //Structure containing the robot parameters. The structure is defined in the Config.h file
 
-
+	int m_M_TO_PX; //Copy of the scale to do the conversion from real dimensions (m) to simulated ones (pixels)
 };
 
-
-
 #endif /* ROBOTCONTROLLER_H_ */
+
