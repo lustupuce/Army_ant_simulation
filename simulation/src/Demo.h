edbafa4cff9e885ab28ee4f20a9c7af8f255e632
lustupuce <luluhouel@hotmail.com>
2019-05-10 12:40:43 +0200

    Update Demo.h

--- simulation/src/Demo.h
+++ simulation/src/Demo.h
@@ -1,8 +1,14 @@
 /*
- * Demo.h
+ *  @file Demo.h
+ *	@brief Implementation of the Demo class and the MyContactListener_v2 class.
+ *	@note The Demo class gather all the elements of the simulation: the terrain, the robot controller and the robots and implement more abstract simulation methods.
+ *	Those methods handle the robot interaction with the terrain and between themselves ie the group behavior.
+ *	This is why the robots are only created via the robot controller.
+ *	The demo class also synchronized the physics and simulation step.
  *
- *  Created on: 6 Nov 2018
- *      Author: lucie
+ *	The MyContactListener_v2 class is used to create a listener on the contact between the physics elements (robots and terrain).
+ *  @date 26 sept. 2018
+ *  @author lucie houel
  */
 
 #ifndef DEMO_H_
@@ -24,7 +30,9 @@
 #include <random>
 #include "BoxTerrain.h"
 
-
+/** @class MyContactListener_v2
+ * This class is a listener on the box2D body contacts. In case of contact it calls the RobotController::findContactRobots(contact) method
+ * */
 class MyContactListener_v2 : public b2ContactListener
 {
 public:
@@ -41,72 +49,155 @@ private:
 	RobotController* m_robotController;
 };
 
+/** @class Demo
+ *	The Demo class gather all the elements of the simulation: the terrain, the robot controller and the robots and implement more abstract simulation methods.
+ *	Those methods handle the robot interaction with the terrain and between themselves ie the group behavior.
+ *	This is why the robots are only created via the robot controller.
+ *	It also handle the simulation results by writing them in two distinct files:
+ *	 - The overall result file with the summary of the results
+ *	 - The bridge file that gather the timestep and position of every robots entering the bridge state. It is updated every time a new robot enter the bridge state.
+ * */
 class Demo {
 public:
 	Demo(b2World* m_world, config::sConfig cfg);
 	virtual ~Demo();
-	void init();
-	void demoLoop();
-	RobotController getController();
-	bool addRobotWithDelay();
-	bool addRobotWithDistance();
-	sf::RenderWindow* getWindow();
-	double getBridgeHeight();
-	double getNewPathLength();
 
-	void captureInThread();
-	void step();
+	/**
+	 * Create a new robot after a given delay defined by m_config.simulation.robot_delay (which has been obtained as an argument of the Demo object creation)
+	 * until the maximum number of robots have been reached.
+	 * The traffic is thus controlled by the initial position and the delay between the robots.
+	 * @return false if the robots are stacking (the previous robot too close to the one that will be created)
+	 */
+	bool addRobotWithDelay();
+	/**
+	 * Create a new robot when a given distance with the previous robot defined by m_config.simulation.robot_distance has been reached
+	 * and the previous robot has an angle corresponding to the desired phase shift.
+	 * It does so until the maximum number of robots have been reached.
+	 * The traffic is thus controlled by the distance between the robots and the phase shift.
+	 * @return true when a new robot is created.
+	 */
+	bool addRobotWithDistance();
+
+	/**
+	 * Create bridge file under m_bridgeFile.
+	 * Every line is structured as follow:
+	 * Timestamp; robot ID; x coordinate; y coordinate; angle; current joint x; current joint y; previous joint x; previous joint y; it entry; age
+	 */
 	void createBridgeFile();
 
-	void writeResultFile();
-	void writeBridgeFile();
+	/**
+	 *Simulation loop that handle the synchronization between the physics and the controller step.
+	 *The visualization is activated or deactivated via m_config.simulation.visualization.
+	 *The Simulation is composed of two steps: the bridge formation where new robots are created following the given delay or distance
+	 *and the bridge dissolution step where no more robots are created.
+	 *The duration of the two steps are determined by respectively m_config.simulation.bridge_duration and m_config.simulation.dissolution_duration.
+	 *The results are written in the two distinct files.
+	 */
+	void demoLoop();
 
+	/**
+	 * Get the bridge height. This function is not up to date and should not be used in this state
+	 */
+	double getBridgeHeight();
+
+	/**
+	 * Get the robot controller object
+	 * Rq: in future development, could be adapted to be a pointer
+	 * @return the robot controller object
+	 */
+	RobotController getController();
+
+	/**
+	 * Get the new path length induced by the bridge from the left corner of the obstacle to its right corner.
+	 * This function is not up to date and should not be used in this state.
+	 */
+	double getNewPathLength();
+
+	/**
+	 * Get a pointer on the SFML window
+	 * @return a pointer on the SFML window
+	 */
+	sf::RenderWindow* getWindow();
+
+	/**
+	 * Initialize the demo
+	 */
+	void init();
+
+	/**
+	 * Single step of the Demo gathering the physics step and the controller step.
+	 */
+	void step();
+
+	/**
+	 * Take a screenshot of the simulation at a given time step.
+	 * The image is saved under  m_config.logfile_path + m_config.logfile_name + "_dissolution_" + std::to_string(m_currentIt) + ".jpg"
+	 * @param draw should be true if the window has to be drawn beforehand. It is the case when the visualization is deactivated
+	 * @param step is the simulation step: it is either 1 if the simulation is in the bridge formation step or 2 if the simulation is in the bridge dissolution one.
+	 */
 	void takeScreenshot(bool draw, int step);
 
+	/**
+	 * Write the file containing the summary of the simulation results
+	 * The file is saved under m_config.logfile_path + m_config.logfile_name + "_result.txt";
+	 */
+	void writeResultFile();
+
+	/**
+	 * Write the file containing the bridge formation details.
+	 * This file is updated every time a new robot enter the bridge state
+	 * The file is saved under m_config.logfile_path + m_config.logfile_name + "_bridge.txt";
+	 */
+	void writeBridgeFile();
+
 private:
-	sf::RenderWindow window;
+	sf::RenderWindow window; //SFML window used to display the simulation
 //	b2Vec2 m_gravity = b2Vec2(0.f, 0.f);
-	RobotController m_robotController;
-	double m_to_px;
-	VStepper m_terrain;
-	MyContactListener_v2* myContactListener;
-	double m_it = 0;
-	int m_nbRobots = 0;
+	RobotController m_robotController; //Robot controller containing the vector of the robots
+	double m_to_px; //Scale to do the conversion from real dimensions (m) to simulated ones (pixels). Usually obtained from the terrain
+	BoxTerrain m_terrain; //Terrain used to do the simulation, the object can be changed to either: Terrain, BoxTerrain, Vterrain, V2BLTerrain, VStepper
+	MyContactListener_v2* myContactListener; //Contact listener
+	double m_it = 0; //Iteration counter used to create the robots with a given delay between them
+	int m_nbRobots = 0; //Number of robots created
 	double m_elapsedTime = 0; // in seconds (real-time =/= from simulation time)
-	double m_elapsedTimeDissolution = 0;
-	double m_elapsedTimeBridge = 0;
-	double m_length = 0;
-	double m_height = 0;
-	int m_currentIt = 0;
-	int m_nbRobotsInBridge = 0;
-	int m_nbRobotsInBridgeState = 0;
+	double m_elapsedTimeDissolution = 0; //Duration of the dissolution phase (s)
+	double m_elapsedTimeBridge = 0; //Duration of the bridge formation phase (s)
+	double m_length = 0; // Bridge length (Body length)
+	double m_height = 0; // Bridge height (Body length)
+	int m_currentIt = 0; // Current iteration
+	int m_nbRobotsInBridge = 0; // Number of robots involved in the bridge structure, ie the ones in bridge state + the ones stuck under the structure
+	int m_nbRobotsInBridgeState = 0; // Number of robots in the bridge state
 
-	bool m_stacking = false;
-	bool m_stableBridge = false;
+	bool m_stacking = false; //Flag to indicate that the robots are stacking
+	bool m_stableBridge = false; //Flag to indicate that a stable bridge has been reached
 
-	b2Vec2 m_startP;
-	b2Vec2 m_endP;
+	b2Vec2 m_startP; //Start point of the bridge (on the left side of the obstacle
+	b2Vec2 m_endP; //End point of the bridge (on the left side of the obstacle
 
-	config::sConfig m_config ;
+	config::sConfig m_config ; //Configuration parameters (cf Config.h file)
 
 //    sf::Texture m_tex;
 
-	std::ofstream m_logFile;
-	std::ofstream m_bridgeFile;
+	std::ofstream m_logFile; //Overall result file
+	std::ofstream m_bridgeFile; //Bridge file that describe the bridge formation
 
+	/**
+	 * Parameters used to implement a gaussian distribution of the delay between the robots
+	 */
 	// random device class instance, source of 'true' randomness for initializing random seed
 	std::random_device m_rd;
 	// Mersenne twister PRNG, initialized with seed from previous random device instance
 	std::mt19937 m_gen;
 	double m_seed;
 	double m_std_dev=0.25; //0.25
-
-//	std::default_random_engine gen;
+	//	std::default_random_engine gen;
 	std::normal_distribution<double> m_gauss_delay;
 
 protected:
-	b2World* m_world = nullptr;
+	b2World* m_world = nullptr; // pointer on the Box2D world
 };
 
+
+#endif /* DEMO_H_ */
 
 #endif /* DEMO_H_ */
