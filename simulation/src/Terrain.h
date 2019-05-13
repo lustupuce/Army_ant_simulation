c7b8ce1ab9219c079f3f65c482a358842ae73fe7
lustupuce <luluhouel@hotmail.com>
2019-05-13 10:51:09 +0200

    Update Terrain.h

--- simulation/src/Terrain.h
+++ simulation/src/Terrain.h
@@ -2,7 +2,8 @@
  *  @file Terrain.h
  *	@brief Implementation of the Terrain class.
  *	@note General class for the Terrain creation including method for the Box2D implementation (Physics) and the SFMLF synchronization (graphics).
- *	The default terrain is linear ground of width 2*m_runaway located at m_posY from the top of the window
+ *	The default terrain is linear ground of width 3*m_runaway located at m_posY from the top of the window.
+ *	The m_width, m_height and m_angle members do not represent actual dimension in the case of the default terrain.
  *  @date 28 sept. 2018
  *  @author lucie houel
  */
@@ -29,7 +30,12 @@ public:
 
 	/*Default terrain is linear ground of width 2*m_runaway located at m_posY from the top of the window*/
 	/** The function create MUST be called if the terrain object has been created via the default constructor (ex when created dynamically with new),
-	 * otherwise no need to use it*/
+	 * otherwise no need to use it
+	 * @param world is a pointer on the Box2D world object
+	 * @param window is the SFML window 
+	 * @param terrainParam are the terrain parameters (cf Config.h)
+	 * @param WINDOW_X_PX is the x-size of the window. it is used to calculate the scale to convert from meters to pixels
+	 * @param bodylength is the size of a robot. it is used to convert the dimension from body length to m */
 	virtual void create(b2World* world, sf::RenderWindow& window, config::sTerrain terrainParam, int WINDOW_X_PX, double bodyLength);
 
 	/**
